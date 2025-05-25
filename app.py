b1268ed5a94bdb3d6c305731d4cf084595adc7ef
npirija2 <pirijanejra0@gmail.com>
2025-05-25 16:56:45 +0200

    Fix Flask app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from fuzzy_system import predict_priority
+import os
 
 app = Flask(__name__)
 
@@ -18,9 +19,7 @@ def index():
         return render_template('index.html', result=result)
     return render_template('index.html', result=None)
 
-
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+   
+    app.run(host='0.0.0.0', port=port, debug=False)
